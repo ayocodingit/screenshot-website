8aabfac6310cbf46a1325ce2a4f6996a49b401fa
ayocodingit <ayocodingit@gmail.com>
2021-09-26 12:48:09 +0700

    dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,11 @@
-FROM registry.digitalservice.id/proxyjds/library/alpine:edge
 
-RUN apk add chromium \
-      nss \
-      freetype \
-      harfbuzz \
-      ca-certificates \
-      ttf-freefont \
-      nodejs \
-      yarn \
-      && rm -rf /var/cache/*
+FROM zenika/alpine-chrome:with-node
 
-WORKDIR /app
-
-COPY . .
-
-ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
-    PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
-
-# Puppeteer v6.0.0 works with Chromium 89.
-RUN yarn install
-
-# Add user so we don't need --no-sandbox.
-RUN addgroup -S pptruser && adduser -S -g pptruser pptruser \
-    && mkdir -p /home/pptruser/Downloads /app /app/tmp \
-    && chown -R pptruser:pptruser /home/pptruser \
-    && chown -R pptruser:pptruser /app
-
-# Run everything after as non-privileged user.
-USER pptruser
-
-EXPOSE 3003
-
-CMD ["yarn", "run", "start"]
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD 1
+ENV PUPPETEER_EXECUTABLE_PATH /usr/bin/chromium-browser
+WORKDIR /usr/src/app
+COPY --chown=chrome package.json package-lock.json ./
+RUN npm install
+COPY --chown=chrome . ./
+ENTRYPOINT ["tini", "--"]
+CMD ["node", "server.js"]
--- utils/screenshot.js
+++ utils/screenshot.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 
 const tagOption = {
   github: {
-    tagUsername: 'input[name=login]',
-    tagPassword: 'input[name=password]',
-    tagSubmit: 'input[type=submit]'
+    tagUsername: '#login_field',
+    tagPassword: '#password',
+    tagSubmit: '[name="commit"]'
   },
   gitlab: {
     tagUsername: '#user_login',
@@ -29,10 +29,9 @@ const gitLogin = async (page, git) => {
   if (await page.$(option.tagUsername) !== null) {
     await page.type(option.tagUsername, process.env.ACCOUNT)
     await page.type(option.tagPassword, Buffer.from(process.env.PASSWORD, 'base64').toString())
-    await Promise.all([
-      page.click(option.tagSubmit),
-      page.waitForNavigation({ waitUntil: 'load' })
-    ])
+    await page.click(option.tagSubmit)
+    console.log('logging ...');
+    await page.waitForNavigation()
   }
 }
 
@@ -72,6 +71,7 @@ const minimal_args = [
   '--password-store=basic',
   '--use-gl=swiftshader',
   '--use-mock-keychain',
+  '--disable-web-securit'
 ]
 
 const blocked_domains = [
@@ -79,13 +79,10 @@ const blocked_domains = [
   'adservice.google.com',
 ];
 
-
 const screenshot = async (url, git, host) => {
   let filePath = generateFilePath()
   const browser = await puppeteer.launch({ args: minimal_args })
   const page = await browser.newPage()
-  await page.setViewport({ height: 1280, width: 1080 })
-  await page.setRequestInterception(true);
   page.on('request', request => {
     const url = request.url()
     if (blocked_domains.some(domain => url.includes(domain))) {
@@ -94,10 +91,15 @@ const screenshot = async (url, git, host) => {
       request.continue();
     }
   });
+  await page.setBypassCSP(true)
+  await page.setViewport({ height: 1280, width: 1080 })
+  await page.setRequestInterception(true);
   await page.goto(url, { waitUntil: 'load' })
   if (git) await gitLogin(page, git)
-  if (await page.url() === url) await page.screenshot({ path: filePath })
-  else filePath = null
+  console.log(await page.url());
+  await page.screenshot({ path: filePath })
+  // if (await page.url() === url) await page.screenshot({ path: filePath })
+  // else filePath = null
   await browser.close()
   return filePath ? host + '/' + filePath : null
 }
