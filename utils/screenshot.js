cfab1536e8747d31e0c1683add0f15bd62288ff6
ayocodingit <ayocodingit@gmail.com>
2021-09-26 10:57:40 +0700

    Added heroku.yml

--- utils/screenshot.js
+++ utils/screenshot.js
@@ -36,11 +36,64 @@ const gitLogin = async (page, git) => {
   }
 }
 
+const minimal_args = [
+  '--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-component-update',
+  '--disable-default-apps',
+  '--disable-dev-shm-usage',
+  '--disable-domain-reliability',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
+  '--disable-setuid-sandbox',
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
+  '--no-first-run',
+  '--no-pings',
+  '--no-sandbox',
+  '--no-zygote',
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain',
+]
+
+const blocked_domains = [
+  'googlesyndication.com',
+  'adservice.google.com',
+];
+
+
 const screenshot = async (url, git, host) => {
   let filePath = generateFilePath()
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-web-security'] })
+  const browser = await puppeteer.launch({ args: minimal_args })
   const page = await browser.newPage()
   await page.setViewport({ height: 1280, width: 1080 })
+  await page.setRequestInterception(true);
+  page.on('request', request => {
+    const url = request.url()
+    if (blocked_domains.some(domain => url.includes(domain))) {
+      request.abort();
+    } else {
+      request.continue();
+    }
+  });
   await page.goto(url, { waitUntil: 'load' })
   if (git) await gitLogin(page, git)
   if (await page.url() === url) await page.screenshot({ path: filePath })
